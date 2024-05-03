4a3b5b6f2341b6cb5acfd88ddd9e61b7ae24f515
dayzorgno <164095538+dayzorgno@users.noreply.github.com>
2024-05-03 19:37:37 -0300

    Add files via upload

---
+++ daiana zorgno/Home/hotel/models.py
@@ -0,0 +1,31 @@
+
+from django.db import models
+
+
+class SalaConferencias(models.Model):
+    nombre = models.CharField(max_length=100)
+    capacidad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Buffet(models.Model):
+    horario = models.CharField(max_length=100)
+    menu = models.TextField()
+
+    def __str__(self):
+        return self.horario
+
+    def save(self, *args, **kwargs):
+       
+        if not self.menu:
+            self.menu = "Fideos, Pollo, Asado, Ensalada"
+        super().save(*args, **kwargs)
+
+class Habitacion(models.Model):
+    horario_admision = models.CharField(max_length=100)
+    camas_disponibles = models.IntegerField(choices=[(2, 'Dos camas'), (4, 'Cuatro camas')])
+
+    def __str__(self):
+        return self.horario_admision
+
